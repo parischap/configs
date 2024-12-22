2431f715d3785468f52e2a2907fa46b39b452abf
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-12-22 22:57:01 +0100

    chore: restaured fake tests

---
+++ tests/fake.test.ts
@@ -0,0 +1,8 @@
+/* eslint functional/no-expression-statements: "off" */
+import { describe, expect, it } from 'vitest';
+
+describe('fake test', () => {
+	it('fake', () => {
+		expect(1).toBe(1);
+	});
+});
